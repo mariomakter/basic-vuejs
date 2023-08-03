df0b9ec8b289e027d76585f6d3bd8d9620f31dbe
mariomakter <mariom9267idb@gmail.com>
2023-08-03 08:31:31 +0600

    computed watch method

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -1,11 +1,11 @@
-
 <template>
   <div class="container">
     <div class="row">
       <div class="col">
-       <h1>{{ course.name }} by using{{ title }}</h1>
+       <h1 @click="changeName">{{ course.name }} by using{{ title }}</h1>
+       <h2>couese by {{ fullName }}</h2>
        <button @click="increment(2)">+</button>
-       <h3>{{ inc }}</h3>
+       <h3>{{ count }}/{{ oddOrEven }}</h3>
        <button @click="decrement(1)">-</button>
       </div>
     </div>
@@ -13,20 +13,33 @@
 </template>
 <!-- ref state -->
 <script setup>
-import {ref, reactive} from 'vue';
+import {ref, reactive, computed, watch} from 'vue';
 let title = ref(' Function/method ');
-const inc = ref(0);
-inc.value += 1;
+const count = ref(0);
 let course = reactive({
   name: 'vuejs',
-  mentor:'Ibrahim Mahmud',
+  mentor_firstName:'Ibrahim',
+  mentor_lastName:'Mahmud',
   platform:'Instructory'
 });
-function increment(amount){
-  inc.value +=amount;
-}
-function decrement(amount){
-  inc.value -=amount;
-}
-</script>
 
+// es6 method using
+const increment = amount => count.value += amount;
+const decrement = amount => count.value -= amount;
+const changeName = () => course.name = "Maria";
+const fullName = computed(()=>{
+return course.mentor_firstName+' '+course.mentor_lastName;
+});
+const oddOrEven = computed(()=>{
+//  (count.value%2==0)? 'even': 'odd';
+ return count.value%2==0 ? 'even': 'odd';
+
+})
+watch(()=>count.value,(newValue,oldValue)=>{
+console.log('newvalue', newValue);
+console.log('oldValue', oldValue);
+if(newValue>10){
+  alert("counter value is over than 10");
+}
+})
+</script>
