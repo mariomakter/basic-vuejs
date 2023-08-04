906f3c3046abb8309e3979e20ede9009609f8f8c
mariomakter <mariom9267idb@gmail.com>
2023-08-04 23:00:44 +0600

    v-show directives

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -2,10 +2,11 @@
   <div class="container">
     <div class="row">
       <div class="col">
-       <h1>{{ course.name }} by using<span v-text="title"></span></h1>
+       <h1 v-show="showBtn">{{ course.name }} by using<span v-text="title"></span></h1>
        <h1 v-html="rawHtml"></h1>
        <h2>couese by {{ fullName }}</h2>
       </div>
+      <button @click="showBtn=!showBtn">show/hide</button>
     </div>
   </div>
 </template>
@@ -15,6 +16,7 @@ import {ref, reactive} from 'vue';
 let title = ref(' Built in directives -  v-tex');
 const count = ref(0);
 let rawHtml = ref('<li>task1</li>');
+let showBtn = ref(false)
 let course = reactive({
   name: 'vuejs',
   mentor_firstName:'Ibrahim',
