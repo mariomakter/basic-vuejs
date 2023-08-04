ac9f51880ff494ff63609034f67352efa3d68852
mariomakter <mariom9267idb@gmail.com>
2023-08-04 23:11:18 +0600

    v-if-else

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -2,8 +2,9 @@
   <div class="container">
     <div class="row">
       <div class="col">
-       <h1 v-show="showBtn">{{ course.name }} by using<span v-text="title"></span></h1>
-       <h1 v-html="rawHtml"></h1>
+       <h1 v-if="showBtn">{{ course.name }} by using<span v-text="title"></span></h1>
+       <h1 v-else-if="!showBtn">{{ showBtn }}</h1>
+       <h1 v-else>no thing happen</h1>
        <h2>couese by {{ fullName }}</h2>
       </div>
       <button @click="showBtn=!showBtn">show/hide</button>
@@ -16,7 +17,7 @@ import {ref, reactive} from 'vue';
 let title = ref(' Built in directives -  v-tex');
 const count = ref(0);
 let rawHtml = ref('<li>task1</li>');
-let showBtn = ref(true)
+let showBtn = ref()
 let course = reactive({
   name: 'vuejs',
   mentor_firstName:'Ibrahim',
