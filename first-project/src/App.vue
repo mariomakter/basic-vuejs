f2a1e09d077b2976ce58ae3ba618b7ba8fc94fdb
mariomakter <mariom9267idb@gmail.com>
2023-08-05 15:05:53 +0600

    v-bind

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -6,18 +6,22 @@
   <h1 v-for="(todo,index) in todoList" :key="index">{{ index }}/{{ todo.todo }}</h1>
     </div>
   </div>
+  <div class="">
+    <img :src="imgurl" alt="">
+  </div>
   </div>
 </template>
 <!-- ref state -->
 <script setup>
 import {ref, reactive} from 'vue';
-let title = ref(' Built in directives -  v-for');
+let title = ref(' Built in directives -  v-bind');
 let showBtn = ref()
 let todoList = ref([
 {id:1, todo:'task1'},
 {id:2, todo:'task2'},
 {id:3, todo:'task3'},
-])
+]);
+let imgurl = ref('https://picsum.photos/200');
 
 
 </script>
