9d73f5d0bcdb956fd590d1d0708529346b7e5c81
mariomakter <mariom9267idb@gmail.com>
2023-08-10 11:01:13 +0600

    nextticks

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -1,11 +1,22 @@
 <template>
     <h1>{{ title }}</h1>
     <input type="text" ref="input">
+    <button id="counter" @click="increment">{{ count }}</button>
 </template>
 <!-- ref state -->
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, nextTick } from 'vue';
 let title = ref('New value');
+let count = ref(0);
+ const increment = async()=>{
+    count.value++;
+
+    //dom update  before state
+    console.log(document.getElementById('counter').textContent);
+    await nextTick();
+    //dom is updated
+    console.log(document.getElementById('counter').textContent);
+}
 const input = ref(null);
 onMounted(()=>{
     input.value.focus()
